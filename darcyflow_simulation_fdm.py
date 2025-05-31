7599a8a688a6f813a3c3fb2c9eb301c5ce7c3f67
Joseph <perryjd@cardiff.ac.uk>
2025-05-31 13:08:42 +0100

    CFL condition correction and BC function compilation

--- darcyflow_simulation_fdm.py
+++ darcyflow_simulation_fdm.py
@@ -40,17 +40,19 @@ VIDEO_SKIP_FRAMES = 0#9_000
 
 ### functions
 
+# type: (jnp.array, jnp.array, float, float, float, float, float) -> jnp.array
 @jax.jit
 def solve_darcy_fdm(h, k, dt, dx, dy, ss, rr):
 	""" Solve 2D Darcy Flow PDE for next state using explicit finite difference method
 	arg: h: jnp.array: 2D array representing current hydraulic head
 	arg: k: jnp.array: 2D array representing hydraulic conductivity
-	args: dt, dx, dy: float: size of discretizations
-	arg: ss: float: specific-storage coefficient
-	arg: rr: float: recharge rate
+	args: dt, dx, dy: float: Size of discretizations
+	arg: ss: float: Specific-storage coefficient
+	arg: rr: float: Recharge rate
 	returns: jnp.array: Next hydraulic head
 	"""
 	
+	# solve for next state
 	laplacian = (
 		(jnp.roll(h, -1, axis=0) + jnp.roll(h, 1, axis=0) - 2*h) / dx**2 +
 		(jnp.roll(h, -1, axis=1) + jnp.roll(h, 1, axis=1) - 2*h) / dy**2
@@ -58,6 +60,22 @@ def solve_darcy_fdm(h, k, dt, dx, dy, ss, rr):
 	next_state = h + dt / ss * (k * laplacian + rr)
 	
 	return next_state
+
+# type: (jnp.array) -> jnp.array
+@jax.jit
+def apply_boundary_conditions(h):
+	
+	# apply edge BCs
+	hhat = h
+	hhat = hhat.at[0, :].set(0.0)
+	hhat = hhat.at[-1, :].set(0.0)
+	hhat = hhat.at[:, 0].set(0.0)
+	hhat = hhat.at[:, -1].set(0.0)
+	
+	# apply pinhole BCs
+	#hhat = hhat.at[*[n//2 for n in h.shape]].set(0.0)
+	
+	return hhat
 
 # type: (jnp.array, jnp.array, int, float, float, float, float, float) -> List[jnp.array]
 def simulate_hydraulic_head_fdm(h, k, n_steps, dt, dx, dy, ss, rr):
@@ -67,7 +85,7 @@ def simulate_hydraulic_head_fdm(h, k, n_steps, dt, dx, dy, ss, rr):
 	assert len(h.shape) == 2, f"ASSERT: Grid must be 2D: shape={h.shape}."
 	
 	# stability check (Courant–Friedrichs–Lewy)
-	cfl_value = jnp.max(k) * dt / (ss * dx**2)
+	cfl_value = jnp.max(k) * dt * (1 / dx**2 + 1 / dy**2) / ss
 	if cfl_value >= 0.25:
 		print(f"WARN: Proceeding with unstable simulation. CFL condition (CFL<0.25) not satisfied (CFL={cfl_value:.3f}), reduce dt or increase dx.\n")
 	
@@ -75,19 +93,8 @@ def simulate_hydraulic_head_fdm(h, k, n_steps, dt, dx, dy, ss, rr):
 	state = h
 	sim_h = [h]
 	for t in tqdm(range(n_steps-1)):
-		
-		# solve for next state
 		state = solve_darcy_fdm(state, k, dt, dx, dy, ss, rr)
-	
-		# apply edge BCs
-		state = state.at[0, :].set(0.0)
-		state = state.at[-1, :].set(0.0)
-		state = state.at[:, 0].set(0.0)
-		state = state.at[:, -1].set(0.0)
-		
-		# apply pinhole BCs
-		#state = state.at[*[x//2 for x in h.shape]].set(0.0)
-		
+		state = apply_boundary_conditions(state)
 		sim_h.append(state)
 	
 	return sim_h
